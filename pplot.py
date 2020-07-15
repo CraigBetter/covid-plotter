6c21395f89f765d9cf8fe8a2ce813b7696623420
U-LPS\cbest <cbest@HAMFWKS23JFCZ1.polysci.com>
2020-07-15 15:14:12 -0300

    last commit was broken

--- pplot.py
+++ pplot.py
@@ -8,12 +8,13 @@ def pplot():
     names = data['prname'].unique()
     return data
 
-if __name__ == "__main__":
-    main()
-
 def main():
     data = pplot()
     NS = data.prname == "Nova Scotia"
     NS_data = data[NS]
     NS_data.plot(x='date',y='numtoday')
     plt.show()
+
+if __name__ == "__main__":
+    main()
+
