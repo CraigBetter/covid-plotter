8a638e7a83efcdb1430f04b96f9a50ec51a1ce9f
U-LPS\cbest <cbest@HAMFWKS23JFCZ1.polysci.com>
2020-09-28 11:28:02 -0300

    Added comments. Line 28 of pcp is confusing, needs to be changed.

--- pcp.py
+++ pcp.py
@@ -3,20 +3,29 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# TODO: 
+# TODO: apend series of total daily cases to dataframe
 
 class pcp():
     def __init__(self):
         self.data      = pd.read_csv('covid19.csv')
         self.names     = self.data['prname'].unique()
-        self.provinces = []
+        self.provinces = [] #names of provinces to be used as keys
         for count, name in enumerate(self.names):
             self.provinces.append(self.data[self.data["prname"] == name])
+
+    def sicktoday(self):
+        #total ( sick - recovered - dead) ?
+        sicktoday = []
+        for count, province in enumerate(self.provinces):
+            foo = self.data['numtotal'] - self.data['numrecover'] - self.data['ratedeaths']
+            sicktoday.append(foo)
+            pass
 
 def main():
     data = pcp()
     #NS = data.prname == "Nova Scotia"
     #NS_data = data[NS]
+    data.data.add(data.data.prname) #wft is this?
     return data
     #NS_data.plot(x='date',y='numtoday')
     #plt.show()
